3c6c459d153a4074f685494d94cc5b50b8434d8c
noarch <noarch@n0ar.ch>
2021-02-08 15:54:34 -0500

    major code refactor
    now queries Telegram for existing invite links rather than using a
    global variable;
    and generally cleaner(?) code

--- fprotect.py
+++ fprotect.py
@@ -7,16 +7,22 @@ This program is released under the public domain with WTFPL.
 You just DO WHAT THE FUCK YOU WANT TO.
 """
 
+import os
+
 from flask import Flask, redirect, request
 from requests import get
 
-# Your Telegram bot API key,
-# this is also the URL FluffyProtect listens to Telegram's webhook
-API_KEY = ''
+# The API key is now set using an environment variable
+try:
+    API_KEY = os.environ['FPROTECT_API_KEY']
+except KeyError:
+    print('FPROTECT_API_KEY not set')
+    exit(1)
+
+# Change this if you use your own Telegram bot API server
+API_LINK = 'https://api.telegram.org/bot{}/'.format(API_KEY)
 
 app = Flask(__name__)
-invite_links = {}
-
 
 @app.route('/' + API_KEY, methods=['POST'])
 def listen_for_updates():
@@ -26,8 +32,7 @@ def listen_for_updates():
         # get the chat from Telegram's request
         chat_id = update['message']['chat']['id']
 
-        global invite_links
-        invite_links[chat_id] = get_invite_link(API_KEY, chat_id)
+        update_link(chat_id)
 
     return('ok')
 
@@ -37,41 +42,32 @@ def client_request(chat_id):
     # Flask passes chat_id as a string
     chat_id = int(chat_id)
 
-    # If no invite link for chat_id has been generated before, generate one now
-    global invite_links
-    try:
-        invite_links[chat_id]
-    except KeyError:
-        invite_links[chat_id] = get_invite_link(API_KEY, chat_id)
-
-    return(redirect(invite_links[chat_id], code=302))
+    return(redirect(get_link(chat_id), code=302))
 
 
-def get_invite_link(api_key, chat_id):
-    """
-    Get invite link for chat_id.
-
-    api_key (str): Your Telegram bot API key
-    chat_id (int): Target chat ID
-    """
-
-    api_link = ('https://api.telegram.org/bot{}/exportChatInviteLink'
-                .format(api_key))
-    api_request = get(api_link,
-                      params={'chat_id': str(chat_id)})
+def query_api(method, chat_id):
+    api_request = get(API_LINK + method,
+                      {'chat_id': str(chat_id)})
 
     api_response = api_request.json()
 
-    # Return and error handling
     if api_response['ok']:
-        new_invite_link = api_response['result']
+        return api_response
     elif (api_response['error_code']):
         # Raise Exception with error given by Telegram
         raise Exception(api_response['description'])
     else:
-        raise Exception('Unexpected error')
+        raise Exception('Unexpected error')    
 
-    return(new_invite_link)
+
+def get_link(chat_id):
+    api_response = query_api('getChat', chat_id)
+
+    return api_response['result']['invite_link']
+
+
+def update_link(chat_id):
+    query_api('exportChatInviteLink', chat_id)
 
 
 # It also runs standalone
